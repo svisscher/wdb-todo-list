8fd736522dcdd8c881f082e3d6f35d8d272c395b
Sabine Visscher <sabine.visscher@gmail.com>
2018-09-03 15:24:42 +0200

    Initial commit basic functionality
    * Add new to-do item
    * Mark to-do item as done
    * Delete to-do item
    * Show/hide input field

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('input').keypress(function (e) {
+  // Trigger on 'Enter' key
+  if (e.which === 13) {
+    // Add new item to list by inserting user input
+    const input = $('input').val();
+    $('ul').append('<li class="item"><span class="delete">X</span>' + input + '</li>');
+    // Empty input field
+    $('input').val('');
+  }
+});
+
+$('#expand').on('click', function () {
+  $('input').fadeToggle('400', 'linear');
+});
+
+$('li.item').on('click', function () {
+  $(this).toggleClass('done');
+});
+
+$('span.delete').on('click', function () {
+  $(this).parent().fadeOut('400', function () {
+    $(this).remove();
+  });
+});
